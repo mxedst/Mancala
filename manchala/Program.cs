762b0ac1985d25a4d92ca6739bf2907676641eb9
Maxwell Stanford <76766108+mxedst@users.noreply.github.com>
2025-05-18 22:55:37 +0330

    Add files via upload

---
+++ manchala/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace manchala
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool inv = false, start=false;
+            string p1 = "PL1", p2 = "PL2";
+            int starter = 4;
+            string menu = "\tMANQALE\n\n" +
+                    "1-Play with a friend\n" +
+                    "2-Play with bot\n" +
+                    "3-How to play\n" +
+                    "4-Credits\n";
+            string underCons= "\n"+
+                        "██    ██ ███    ██ ██████  ███████ ██████       ██████  ██████  ███    ██ ███████ ████████ ██████  ██    ██  ██████ ████████ ██  ██████  ███    ██ \n"+
+                        "██    ██ ████   ██ ██   ██ ██      ██   ██     ██      ██    ██ ████   ██ ██         ██    ██   ██ ██    ██ ██         ██    ██ ██    ██ ████   ██ \n"+
+                        "██    ██ ██ ██  ██ ██   ██ █████   ██████      ██      ██    ██ ██ ██  ██ ███████    ██    ██████  ██    ██ ██         ██    ██ ██    ██ ██ ██  ██ \n"+
+                        "██    ██ ██  ██ ██ ██   ██ ██      ██   ██     ██      ██    ██ ██  ██ ██      ██    ██    ██   ██ ██    ██ ██         ██    ██ ██    ██ ██  ██ ██ \n"+
+                        " ██████  ██   ████ ██████  ███████ ██   ██      ██████  ██████  ██   ████ ███████    ██    ██   ██  ██████   ██████    ██    ██  ██████  ██   ████ \n"+
+                        "                                                                                                                                                   \n"+
+                        "0-return to the main menu";
+            string HowToPlay = "\tHOW TO PLAY\nSet Up: Place 4 stones in each of the 12 round spaces.\n " +
+                        "Leave the larger ovals (called the ‘Store’) empty.\n\n" +
+                        "First player chooses a house on their own side(right), picks up the stones in that space\n" +
+                        "and starting with the next space, drops one stone in each space going counterclockwise(including the Store)\n" +
+                        "until all of the stones are played out. \n\n" +
+                        "Now it is the other player’s turn to do the same using stones on their side and continuing to play counterclockwise\n\n" +
+                        " * Note: if there are enough stones to reach the other player’s \"Store\"(not house), skip that space and continue on your own.\n\n" +
+                        "If your last stone goes into your Store, you get to go again.\n\n" +
+                        "If your last stone goes into an empty space on \"your\" side, move that stone AND\n" +
+                        "any captured stones ( those directly across on your opponent’s side) to your Store.\n\n" +
+                        "The game ends when either player has no more stones in their six circles. The remaining stones go to the other player’s Store.\n\n" +
+                        "The winner is the player with the most stones in their Store\n\n" +
+                        "0-return to the main menu";
+            string credits = "credits";
+            string whatToShow = menu;
+            
+
+            ConsoleKey key;
+            do
+            {
+                Console.Clear();
+                Console.Write(whatToShow);
+                if (inv)
+                {
+                    Console.Write("\n\n *** INVALID INPUT ***\n");
+                    inv = false;
+                }
+                if (start)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"enter first player's name:   (default is '{p1}')");
+                    string input = Console.ReadLine();
+                    if (input.Length > 0)
+                        p1 = input;
+                    Console.Clear();
+                    Console.WriteLine($"enter second player's name:   (default is '{p2}')");
+                    input = Console.ReadLine();
+                    if (input.Length > 0)
+                        p2 = input;
+                    Console.Clear();
+                    Console.WriteLine($"how many stones do you want to play with? we recommend 3 or 4   (default is '{starter}')");
+                    input = Console.ReadLine();
+                    try
+                    {
+                        starter = Convert.ToInt32(input);
+                    }
+                    catch (Exception e) {
+                        Console.WriteLine("then 4 it is.");
+                        Console.WriteLine("press any key to continue");
+                        Console.ReadKey();
+                    }
+                    break;
+                }
+                /*while (!Console.KeyAvailable)
+                {
+                }*/
+
+                // Key is available - read it
+                key = Console.ReadKey(true).Key;
+                start = false;
+                switch (key)
+                {
+                    case ConsoleKey.NumPad1:
+                        start = true;
+                        break;
+                    case ConsoleKey.NumPad2:
+                        whatToShow = underCons;
+                        break;
+                    case ConsoleKey.NumPad3:
+                        whatToShow = HowToPlay;
+                        break;
+                    case ConsoleKey.NumPad4:
+                        whatToShow = credits;
+                        break;
+                    case ConsoleKey.NumPad0:
+                        whatToShow = menu;
+                        break;
+                    default:
+                        inv = true;
+                        break;
+                }
+            } while (key != ConsoleKey.Escape);
+
+            Manqale game = new Manqale(p1, p2, starter);
+            game.Play();
+
+        }
+    }
+
+}
---
+++ manchala/manqale.cs
@@ -0,0 +1,235 @@
+﻿using System;
+
+
+namespace manchala
+{
+    class Manqale
+    {
+        string firstP, secondP;
+        int starter;
+        int move;
+        public Manqale(string arg1, string arg2,int arg3)
+        {
+            firstP = arg1;
+            secondP = arg2;
+            starter = arg3;
+        }
+        public void Play()
+        {
+            Game();
+        }
+        private void Game()
+        {
+            int[] MB = new int[14];
+            int[] limits = new int[2];
+            int beans, target, skipper, rightLine, leftLine;
+            bool is_our_turn = true;
+            bool game_over = false;
+            int place;
+            for (int i = 1; i <= 14; i++)
+                MB[i - 1] = starter;
+            MB[6] = MB[13] = 0;
+
+            string shortName1 = firstP, shortName2 = secondP;
+            if (shortName1.Length > 3)
+                shortName1 = shortName1.Remove(3);
+            if (shortName2.Length > 3)
+                shortName2 = shortName2.Remove(3);
+
+
+            while (!game_over)
+            {
+                ShowBoard(MB, shortName1.ToUpper().ToCharArray(), shortName2.ToUpper().ToCharArray());
+
+
+                leftLine = rightLine = 0;
+                for (int i = 0; i < 6; i++)
+                    rightLine += MB[i];
+                for (int i = 7; i < 13; i++)
+                    leftLine += MB[i];
+                if (is_our_turn && rightLine == 0)
+                {
+                    MB[13] += leftLine;
+                    for (int i = 7; i < 13; i++)
+                        MB[i] = 0;
+                    game_over = true;
+                }
+                else if (!is_our_turn && leftLine == 0)
+                {
+                    MB[6] += leftLine;
+                    for (int i = 0; i < 6; i++)
+                        MB[i] = 0;
+                    game_over = true;
+                }
+
+                if (game_over)
+                {
+                    ShowBoard(MB,shortName1.ToUpper().ToCharArray(), shortName2.ToUpper().ToCharArray());
+                    if (MB[13] > MB[6]) {
+                        Console.Write(secondP);
+                        Console.WriteLine(" wins!"); 
+                    }
+                    else if (MB[13] < MB[6])
+                    {
+                        Console.Write(firstP);
+                        Console.WriteLine(" wins!");
+                    }
+                    else Console.WriteLine("Draw!");
+                    continue;
+                }
+
+                if (is_our_turn)
+                {
+                    Console.Write(firstP);
+                    Console.WriteLine(" to move:");
+                }
+                else
+                {
+                    Console.Write(secondP);
+                    Console.WriteLine(" to move:");
+                }
+
+                getMove(MB,is_our_turn);
+                
+                
+                if (is_our_turn)
+                {
+                    beans = MB[move - 1];
+                    MB[move - 1] = 0;
+                    target = 6;
+                    skipper = 13;
+                    limits[0] = 0;
+                    limits[1] = 5;
+                }
+                else
+                {
+                    move = 14 - move;
+                    beans = MB[move - 1];
+                    MB[move - 1] = 0;
+                    target = 13;
+                    skipper = 6;
+                    limits[0] = 7;
+                    limits[1] = 12;
+                }
+                for (int i = beans; i > 0; i--)
+                {
+                    move++;
+                    place = (move - 1) % 14;
+                    if (place == skipper)
+                    {
+                        i++;
+                        continue;
+                    }
+                    MB[place]++;
+
+                    if (i == 1 && place == target)
+                        is_our_turn = !is_our_turn;
+                    else if (i == 1 && MB[place] == 1 && limits[0] <= place && place <= limits[1] && MB[12 - place] > 0)
+                    {
+                        MB[target] += MB[place];
+                        MB[target] += MB[12 - place];
+                        MB[place] = 0;
+                        MB[12 - place] = 0;
+                    }
+                }
+                is_our_turn = !is_our_turn;
+            }
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey();
+            System.Diagnostics.Process.Start("manchala.exe");
+        }
+
+        void getMove(int[] mancala_board, bool is_our_turn,bool rep=false)
+        {
+            int a = 0;
+            if(rep)
+            {
+                Console.Write("invalid input,try again\n");
+            }
+            try
+            {
+                a = Convert.ToInt16(Console.ReadLine());
+                rep = false;
+            }
+            catch(Exception e)
+            {
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                ClearCurrentConsoleLine();
+                rep = true;
+                getMove(mancala_board, is_our_turn, rep);
+                return;
+            }
+
+            if (rep)
+            {
+                Console.SetCursorPosition(0, Console.CursorTop - 2);
+                ClearCurrentConsoleLine();
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                ClearCurrentConsoleLine();
+            }
+
+            if (a > 0 && a < 7)
+            {
+                if ((is_our_turn && mancala_board[a - 1] != 0) || (!is_our_turn && mancala_board[14 - a - 1] != 0))
+                {
+                    move = a;
+                    rep = false;
+                }
+                else
+                    rep = true;
+            }
+            else
+                rep = true;
+
+            if (rep) getMove(mancala_board, is_our_turn, rep);
+
+        }
+
+        private void ClearCurrentConsoleLine()
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+        void ShowBoard(int[] mancala_board, char[] firstP, char[] secondP)
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t|\t\t\t\t|");
+            Console.Write("\t");
+            Console.Write(firstP);
+            Console.Write("\t|\t\t" + mancala_board[6] + "\t\t|\n");
+            Console.WriteLine("\t\t|\t\t\t\t|");
+            Console.WriteLine("\t\t|-------------------------------|");
+            Console.WriteLine("\t\t|\t\t\t\t|");
+            Console.WriteLine("\t6\t|\t" + mancala_board[7] + "\t|\t" + mancala_board[5] + "\t|");
+            Console.WriteLine("\t\t|\t\t\t\t|");
+            Console.WriteLine("\t\t|-------------------------------|");
+            Console.WriteLine("\t\t|\t\t\t\t|");
+            Console.WriteLine("\t5\t|\t" + mancala_board[8] + "\t|\t" + mancala_board[4] + "\t|");
+            Console.WriteLine("\t\t|\t\t\t\t|");
+            Console.WriteLine("\t\t|-------------------------------|");
+            Console.WriteLine("\t\t|\t\t\t\t|");
+            Console.WriteLine("\t4\t|\t" + mancala_board[9] + "\t|\t" + mancala_board[3] + "\t|");
+            Console.WriteLine("\t\t|\t\t\t\t|");
+            Console.WriteLine("\t\t|-------------------------------|");
+            Console.WriteLine("\t\t|\t\t\t\t|");
+            Console.WriteLine("\t3\t|\t" + mancala_board[10] + "\t|\t" + mancala_board[2] + "\t|");
+            Console.WriteLine("\t\t|\t\t\t\t|");
+            Console.WriteLine("\t\t|-------------------------------|");
+            Console.WriteLine("\t\t|\t\t\t\t|");
+            Console.WriteLine("\t2\t|\t" + mancala_board[11] + "\t|\t" + mancala_board[1] + "\t|");
+            Console.WriteLine("\t\t|\t\t\t\t|");
+            Console.WriteLine("\t\t|-------------------------------|");
+            Console.WriteLine("\t\t|\t\t\t\t|");
+            Console.WriteLine("\t1\t|\t" + mancala_board[12] + "\t|\t" + mancala_board[0] + "\t|");
+            Console.WriteLine("\t\t|\t\t\t\t|");
+            Console.WriteLine("\t\t|-------------------------------|");
+            Console.WriteLine("\t\t|\t\t\t\t|");
+            Console.Write("\t");
+            Console.Write(secondP);
+            Console.Write("\t|\t\t" + mancala_board[13] + "\t\t|\n");
+            Console.WriteLine("\t\t|\t\t\t\t|");
+        }
+    }
+}
